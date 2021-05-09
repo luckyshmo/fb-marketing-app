dc013690bad794a276e679ee713cf2878d6968e9
Mikhail Shevelev <shevelevmo@sgaz.pro>
2021-05-09 16:15:04 +0500

    Add gloab styles and second page

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,51 +12,5 @@ export default {
 }
 </script>
 <style>
-#main-button {
-    color:whitesmoke;
-    background-color: #6C1BD2;
-    border-radius: 8px;
-    padding: 12px 28px;
-    border:none;
-    font-family: Montserrat;
-    font-style: normal;
-    font-weight: 600;
-}
-
-#content {
-    padding: 40px;
-}
-
-#h1 {
-  margin-top: 0px;
-  font-family: Montserrat;
-  font-style: normal;
-  font-weight: 900;
-  font-size: 66px;
-  line-height: 100%;
-
-  color: #000000;
-}
-#p1 {
-  font-family: Montserrat;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 20px;
-  line-height: 32px;
-
-  color: #000000;
-
-}
-
-#h2 {
-    margin-top: 50px;
-  font-family: Montserrat;
-  font-style: normal;
-  font-weight: 900;
-  font-size: 36px;
-  line-height: 44px;
-
-  color: #000000;
-}
 
 </style>
