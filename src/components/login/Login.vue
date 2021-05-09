d4cfe5e89a1ea158798de08f1721c554b645018a
Mikhail Shevelev <shevelevmo@sgaz.pro>
2021-05-09 16:14:40 +0500

    Create login and register templates

---
+++ src/components/login/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 id="h1">Вход</h1>
+        <b-button variant="primary" id="main-button">Войти</b-button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
