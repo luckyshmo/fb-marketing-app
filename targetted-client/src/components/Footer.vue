be83d83ff8e9ca1f379dc9c116362da4a972f73d
Mikhail Shevelev <shevelevmo@sgaz.pro>
2021-06-14 16:07:29 +0300

    Add temp opherta

--- targetted-client/src/components/Footer.vue
+++ targetted-client/src/components/Footer.vue
@@ -1,7 +1,14 @@
 <template>
+<div>
     <div class="footer">
-        <p1 id="footer-t">© targetted.ru, 2020–2021</p1>
+        <div id="footer-t">© targetted.ru, 2020–2021</div>
     </div>
+    <div id="footer-of">
+        <a  target="_blank" href="https://drive.google.com/file/d/1lCQy-RaWsdqjq6PVXAPyskqetQXat8ZZ/view?usp=sharing">Оферта</a>
+        <br>ИП Лукьянов Александр Дмитриевич,<br> ИНН: 667904724580,<br> ОГРН: 318665800136408<br>
+        Телефон для связи: +79959862916;<br> почтовый адрес для связи: <a target=”_blank” href="mailto: targettedru@gmail.com">targettedru@gmail.com</a>
+    </div>
+</div>
 </template>
 <script>
 export default {
@@ -12,6 +19,13 @@ export default {
 #footer-t{
     position:relative;
     bottom: -38px;
+    margin: 0 auto;
+}
+#footer-of{
+    margin: 0 auto;
+    margin-top: -80px;
+    width: 1220px;
+    color: #767676;
 }
 .footer {
     height: 100px;
