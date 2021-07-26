261464203efbd36293ad1152e857b71a334e408c
[Partner] Mikhail Shevelev <m.shevelev@andersenlab.com>
2021-07-27 04:35:10 +0500

    remove aferta

--- targetted-client/src/components/Footer.vue
+++ targetted-client/src/components/Footer.vue
@@ -5,11 +5,6 @@
         © targetted.ru, 2020–2021
       </div>
     </div>
-    <!-- <div id="footer-of">
-        <a  target="_blank" href="https://drive.google.com/file/d/1lCQy-RaWsdqjq6PVXAPyskqetQXat8ZZ/view?usp=sharing">Оферта</a>
-        <br>ИП Лукьянов Александр Дмитриевич,<br> ИНН: 667904724580,<br> ОГРН: 318665800136408<br>
-        Телефон для связи: +79959862916;<br> почтовый адрес для связи: <a target=”_blank” href="mailto: targettedru@gmail.com">targettedru@gmail.com</a>
-    </div> -->
   </div>
 </template>
 <script>
